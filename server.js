d8045af68ef31aff07346247cbfdc91e4be5de4a
wxyzcctv <2846629226@qq.com>
2018-12-16 11:58:19 +0800

    前端与后端的数据交互

---
+++ server.js
@@ -0,0 +1,44 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+    var parsedUrl = url.parse(request.url, true)
+    var pathWithQuery = request.url 
+    var queryString = ''
+    if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    var path = parsedUrl.pathname
+    var query = parsedUrl.query
+    var method = request.method
+    if(path === "/"){
+        var string = fs.readFileSync('./index.html','utf8')
+        var amount = fs.readFileSync('./data','utf8')
+        string = string.replace('&&&amount&&&' ,amount)
+        response.setHeader('Content-Type','text/html;charset=utf-8')
+        response.write(string)
+        response.end()
+    }else if(path === '/pay'){
+        var amount = fs.readFileSync('./data','utf8')
+        var newAmount = amount - 1
+        fs.writeFileSync('./data',newAmount)
+        response.setHeader('Content-Type','application/javascript')
+        response.statusCode = 200
+        response.write(`
+            ${query.callback}.call(undefined,'success')
+        `)
+        response.end()
+    }else{
+        response.statusCode = 404
+        response.setHeader('Content-Type','text/html;charset=utf-8')
+        response.write('Unfind the path, you need change the server.js')
+        response.end()
+    }
+})  
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
